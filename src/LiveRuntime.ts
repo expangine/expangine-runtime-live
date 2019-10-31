76cb9765fc2d1b479ce0f2f05d97ddd67aca4013
Philip Diffenderfer <pdiffenderfer@gmail.com>
2019-10-31 12:54:07 -0400

    Account for returned value

--- src/LiveRuntime.ts
+++ src/LiveRuntime.ts
@@ -1,4 +1,4 @@
-import { Runtime, Command, defs } from 'expangine-runtime';
+import { Runtime, Command, defs, Expression, CommandProvider } from 'expangine-runtime';
 
 
 
@@ -10,4 +10,37 @@ export type LiveCommand = Command<LiveContext, LiveResult>;
 
 export type LiveCommandMap<K extends string | number | symbol = string> = Record<K, LiveCommand>;
 
-export const LiveRuntime = new Runtime<LiveContext, LiveResult>(defs);
+export type LiveProvider = CommandProvider<LiveContext, LiveResult>;
+
+export class LiveRuntimeImpl extends Runtime<LiveContext, LiveResult>
+{
+
+  public constructor()
+  {
+    super(defs);
+  }
+
+  public wrapCommandWithReturn(cmd: LiveCommand): LiveCommand
+  {
+    return (context) => {
+      const result = cmd(context);
+
+      return this.returnProperty in context
+        ? context[this.returnProperty]
+        : result;
+    };
+  }
+
+  public getCommandWithReturn(expr: Expression, provider: LiveProvider = this): LiveCommand
+  {
+    return this.wrapCommandWithReturn(this.getCommand(expr, provider));
+  }
+
+  public run(expr: any, context: LiveContext, provider: LiveProvider = this): LiveResult
+  {
+    return this.getCommandWithReturn(this.defs.getExpression(expr), provider)(context);
+  }
+
+}
+
+export const LiveRuntime = new LiveRuntimeImpl();
--- typings/LiveRuntime.d.ts
+++ typings/LiveRuntime.d.ts
@@ -1,6 +1,13 @@
-import { Runtime, Command } from 'expangine-runtime';
+import { Runtime, Command, Expression, CommandProvider } from 'expangine-runtime';
 export declare type LiveContext = Record<string, any> | (any[] & Record<string, any>);
 export declare type LiveResult = any;
 export declare type LiveCommand = Command<LiveContext, LiveResult>;
 export declare type LiveCommandMap<K extends string | number | symbol = string> = Record<K, LiveCommand>;
-export declare const LiveRuntime: Runtime<LiveContext, any>;
+export declare type LiveProvider = CommandProvider<LiveContext, LiveResult>;
+export declare class LiveRuntimeImpl extends Runtime<LiveContext, LiveResult> {
+    constructor();
+    wrapCommandWithReturn(cmd: LiveCommand): LiveCommand;
+    getCommandWithReturn(expr: Expression, provider?: LiveProvider): LiveCommand;
+    run(expr: any, context: LiveContext, provider?: LiveProvider): LiveResult;
+}
+export declare const LiveRuntime: LiveRuntimeImpl;
